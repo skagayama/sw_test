a94d907c7c121612d81a68055ad4b86e50d78bdb
skagayama <s-kagayama@artec-kk.co.jp>
2020-10-26 01:02:36 +0900

    register files

---
+++ swtest/sample/sw.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    './index.html'
+];
+
+self.addEventListener('install', function (event) {
+    console.log('sw.js install');
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('sw.js fetch');
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
